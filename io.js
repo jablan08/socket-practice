0de7e75fcd692001c00e1b3617d40b6b7bff1566
Jablan <joshuaablan@gmail.com>
2019-09-23 14:46:47 -0700

    add players list to server and refactor do/while in app.js

--- io.js
+++ io.js
@@ -1,7 +1,16 @@
 const io = require("socket.io")();
 
 io.on("connection", function(socket) {
+  socket.on('disconnect', function () {
+    delete players[socket.id];
+    io.emit('update-player-list', Object.values(players));
+  });
   // socket represent the currently connecting browser
+  socket.on('register-player', function (initials) {
+    // each socket has a unique id
+    players[socket.id] = initials;
+    io.emit('update-player-list', Object.values(players));
+  });
   socket.on("add-circle", (data) => {
     io.emit("add-circle", data);
   });
@@ -9,6 +18,8 @@ io.on("connection", function(socket) {
     io.emit("clear-circles");
   });
 });
+
+var players = {};
 
 module.exports = io;
 
